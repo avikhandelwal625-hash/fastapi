4def6fc50c16e6d0685377baa98135041e0d6fe3
avikhandelwal625-hash <avikhandelwal625@gmail.com>
2026-01-16 14:00:25 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel 
+class product(BaseModel):
+    id:int
+    name:str
+    description: str
+    price: float
+    quantity:int
+    # def __init__(self,id,name,description,price,quantity):
+    #     self.id=id
+    #     self.name=name
+    #     self.description=description
+    #     self.price=price
+    #     self.quantity=quantity
